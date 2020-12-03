c9ccbfa9355b924a1a0ac8777186ef73eb785e06
Paolo Galli <paologalli@MacBook-Pro-di-Paolo.local>
2020-12-03 15:31:12 +0100

    Added skip button only for the checker

--- src/container/BottomCardButtons.tsx
+++ src/container/BottomCardButtons.tsx
@@ -6,11 +6,12 @@ interface Props {
     onCorrect: () => void;
     onError: () => void;
     onNewTurn: () => void;
+    skipCounter: number;
     newButtonDisabled: boolean;
     pointButtonsDisabled: boolean;
 }
 
-const BottomCardButtons = ({ onSkip, onCorrect, onError, onNewTurn, newButtonDisabled, pointButtonsDisabled }: Props) => {
+const BottomCardButtons = ({ onSkip, onCorrect, onError, onNewTurn, newButtonDisabled, pointButtonsDisabled, skipCounter }: Props) => {
     return (
         <div className="panel">
             <button type="button" className="btn btn-primary btn-circle" disabled={!newButtonDisabled} onClick={() => onNewTurn()}>
@@ -21,7 +22,9 @@ const BottomCardButtons = ({ onSkip, onCorrect, onError, onNewTurn, newButtonDis
             </button>
             <button type="button" className="btn btn-info btn-circle"><i className="fa fa-check"></i>
             </button>
-            <button type="button" className="btn btn-warning btn-circle"><i className="fa fa-times"></i>
+            <button type="button" className="btn btn-warning btn-circle" disabled={pointButtonsDisabled} onClick={() => onSkip()}>
+                Skip
+                <p>{skipCounter}</p>
             </button>
             <button type="button" className="btn btn-danger btn-circle" onClick={() => onError()} disabled={pointButtonsDisabled}>
                 X
